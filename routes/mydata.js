e74187e8baa7fb09e9d9746e3fa32fe321b06790
Bindukattabattula <89418288+Bindukattabattula@users.noreply.github.com>
2021-10-27 00:51:26 -0500

    Update mydata.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Bindu Shiva Sri SaiLakshmi Kattabattula' });
+  res.render('mydata', { title: 'Bindu Shiva Sai Sri Lakshmi Kattabattula' });
 });
 
 module.exports = router;
